6b49c63ef55228ef2e87a128f427c874ccdf2724
João Gabriel Zeba de Souza <112660735+zebajoao@users.noreply.github.com>
2024-08-12 17:16:34 -0300

    Update main.c

--- E2/main.c
+++ E2/main.c
@@ -1,2 +1,17 @@
 /* main.c */
 
+
+int main(int argc, char**argv) {
+
+	while(token = yylex()) {
+        if(token != EOL) {
+            if(token == NUMBER) {
+                printf("constante decimal: %d\n", decimal);
+            }
+            else {
+                printf("código do token: %d\n", token);
+            }
+        }
+    }
+    printf("código do token: %d\n", token);
+}
